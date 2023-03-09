28972a8d002a93a60e6da4fea8595d258361c46b
hnmr293 <hnmr293@gmail.com>
2023-03-09 20:32:49 +0900

    fix #4

--- javascript/llul.js
+++ javascript/llul.js
@@ -1,8 +1,8 @@
-(function() {
-    
+(function () {
+
     if (!globalThis.LLuL) globalThis.LLuL = {};
     const LLuL = globalThis.LLuL;
-    
+
     function id(type, s) {
         return `llul-${type}-${s}`;
     }
@@ -27,15 +27,15 @@
             w = +canvas.width,
             h = +canvas.height;
         if (x < 0) x = 0;
-        if (w < x+w/mm) x = Math.floor(w-w/mm);
+        if (w < x + w / mm) x = Math.floor(w - w / mm);
         if (y < 0) y = 0;
-        if (h < y+h/mm) y = Math.floor(h-h/mm);
-        
+        if (h < y + h / mm) y = Math.floor(h - h / mm);
+
         canvas.dataset.x = x;
         canvas.dataset.y = y;
         canvas.dataset.m = m;
     }
-    
+
     function draw(canvas) {
         const
             x = +canvas.dataset.x,
@@ -44,22 +44,22 @@
             mm = Math.pow(2, m),
             w = +canvas.width,
             h = +canvas.height;
-        
+
         const ctx = canvas.getContext('2d');
-        
+
         const bgcolor = isDark() ? 'black' : 'white';
         ctx.fillStyle = bgcolor;
         ctx.fillRect(0, 0, +canvas.width, +canvas.height);
-        
+
         ctx.fillStyle = 'gray';
-        ctx.fillRect(x, y, Math.floor(w/mm), Math.floor(h/mm));
+        ctx.fillRect(x, y, Math.floor(w / mm), Math.floor(h / mm));
     }
 
     async function update_gradio(type, canvas) {
         await LLuL.js2py(type, 'x', +canvas.dataset.x * M);
         await LLuL.js2py(type, 'y', +canvas.dataset.y * M);
     }
-    
+
     function init(type) {
         const $ = x => gradioApp().querySelector(x);
         const cont = $('#' + id(type, 'container'));
@@ -71,14 +71,16 @@
 
         const width = $(`#${type}_width input[type=number]`);
         const height = $(`#${type}_height input[type=number]`);
-        
+        const width2 = $(`#${type}_width input[type=range]`);
+        const height2 = $(`#${type}_height input[type=range]`);
+
         const canvas = document.createElement('canvas');
         canvas.style.border = '1px solid gray';
         canvas.dataset.x = Math.floor(+width.value / 4 / M);
         canvas.dataset.y = Math.floor(+height.value / 4 / M);
         canvas.dataset.m = m.value;
 
-        for (let ele of [width, height, m, ms]) {
+        for (let ele of [width, height, width2, height2, m, ms]) {
             ele.addEventListener('input', e => {
                 canvas.dataset.m = +m.value;
                 setSize(canvas, width.value, height.value);
@@ -119,12 +121,12 @@
 
         return true;
     }
-    
+
     onUiUpdate(() => {
         if (!LLuL.txt2img) {
             LLuL.txt2img = init('txt2img');
         }
-        
+
         if (!LLuL.img2img) {
             LLuL.img2img = init('img2img');
         }
