62947c1bf3728f1ed3f8bbdcfd5cc09d96af073a
hnmr293 <hnmr293@gmail.com>
2023-05-03 09:20:37 +0900

    update UI

--- scripts/llul.py
+++ scripts/llul.py
@@ -32,12 +32,14 @@ class Script(scripts.Script):
         with gr.Group():
             with gr.Accordion(NAME, open=False, elem_id=id('accordion')):
                 enabled = gr.Checkbox(label='Enabled', value=False)
-                multiply = gr.Slider(value=1, minimum=1, maximum=5, step=1, label='Multiplication (2^N)', elem_id=id('m'))
-                weight = gr.Slider(minimum=-1, maximum=2, value=0.15, step=0.01, label='Weight')
+                with gr.Row():
+                    weight = gr.Slider(minimum=-1, maximum=2, value=0.15, step=0.01, label='Weight')
+                    multiply = gr.Slider(value=1, minimum=1, maximum=5, step=1, label='Multiplication (2^N)', elem_id=id('m'))
                 gr.HTML(elem_id=id('container'))
                 
-                use_mask = gr.Checkbox(value=False, label='Enable mask which scales the weight (black = 0.0, white = 1.0)')
-                mask = gr.File(interactive=True, label='Upload mask image', elem_id=id('mask'))
+                with gr.Row():
+                    use_mask = gr.Checkbox(value=False, label='Enable mask which scales the weight (black = 0.0, white = 1.0)')
+                    mask = gr.File(interactive=True, label='Upload mask image', elem_id=id('mask'))
                 
                 force_float = gr.Checkbox(label='Force convert half to float on interpolation (for some platforms)', value=False)
                 understand = gr.Checkbox(label='I know what I am doing.', value=False)
